b4c56a9feb14806bef41c520b71f03a0b24d1f70
MD Raza <raza231198@gmail.com>
2019-09-25 13:41:16 +0530

    X00P: device cleanup
    Signed-off-by: MD Raza <raza231198@gmail.com>

--- omni_X00P.mk
+++ omni_X00P.mk
@@ -15,11 +15,7 @@
 #
 
 # Inherit Target Product stuff
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
-# Specify phone tech before including full_phone
-$(call inherit-product, vendor/omni/config/gsm.mk)
+$(call inherit-product, build/target/product/embedded.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
